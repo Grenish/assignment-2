14bc9f51299c1900f638958e63ddbd1d2b1a50a8
Grenish rai <ariestheracer@gmail.com>
2024-01-19 01:35:29 +0530

    updated index.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const elevator = document.getElementById("currentFloor");
 const secondFloor = document.getElementById("second-floor");
 const firstFloor = document.getElementById("first-floor");
 const groundFloor = document.getElementById("ground-floor");
+const screen = document.getElementById("screen");
 
 let currentFloor = 1;
 
@@ -11,6 +12,7 @@ function goToFloor(floor) {
   let floorDifference = Math.abs(currentFloor - floor);
   elevator.style.transitionDuration = floorDifference === 1 ? "5s" : "10s";
   currentFloor = floor;
+  screen.innerHTML = currentFloor;
   elevator.style.transform = `translateY(-${(currentFloor - 1) * 247}px)`;
 }
 
